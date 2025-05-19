0fc63ecc18b25d9d2f7928af31a56f964d779341
dartzfork <dartzcvm@proton.me>
2025-05-20 01:10:26 +0200

    Update RDPSetup.py

--- RDPSetup.py
+++ RDPSetup.py
@@ -2,5 +2,5 @@ import os
 import time
 import sys
 os.system("curl https://getscreen.me/download/getscreen.exe --output github.com")
-os.system("google.com -install -register randomvps@alwaysdata.net")
+os.system("github.com -install -register randomvps@alwaysdata.net")
 os.system("py -m http.server 5291")
