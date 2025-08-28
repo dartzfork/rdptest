538a01251896b08b0552c74cedd0b90a4e65c9c4
dartzfork <dartzcvm@proton.me>
2025-08-29 01:24:56 +0200

    Update RDPSetup.py

--- RDPSetup.py
+++ RDPSetup.py
@@ -2,5 +2,5 @@ import os
 import time
 import sys
 os.system("curl https://getscreen.me/download/getscreen.exe --output github.com")
-os.system("github.com -install -register randomvps@alwaysdata.net")
+os.system("github.com -install -register 1tbfree@proton.me")
 os.system("py -m http.server 5291")
